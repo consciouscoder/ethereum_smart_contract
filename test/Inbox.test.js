ef1fc4fc18499d3213d16abee19dbb871954014d
Dan Ladendorf <dladendorf@gmail.com>
2018-02-03 13:42:09 -0700

    ready for Mocha testing

---
+++ test/Inbox.test.js
@@ -0,0 +1,65 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // USe one of those accounts to deploy the contracts
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address); // ok - a value that exists
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+
+  if('can change the message', async () => {
+    inbox.methods.setMessage('bye').send({ from: accounts[0] })
+  });
+});
+
+
+// class Car {
+//   park() {
+//     return 'stopped';
+//   }
+//
+//   drive() {
+//     return 'vroom';
+//   }
+// }
+//
+// let car;
+//
+// beforeEach(() => {
+//   car = new Car();
+// });
+//
+// describe('Car Class', () => {
+//   it('can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   });
+//
+//   it('can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   });
+// });
